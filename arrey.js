4d1be93a7f585794af215145f4be8cd107ba6d9e
raselASrock <mdraselm413@gmail.com>
2022-08-13 00:46:40 +0600

    arrey-added

---
+++ arrey.js
@@ -0,0 +1,15 @@
+// const products = [
+//     {name: "phone", price: 12000} ,
+//     {name: "laptop", price: 32000} ,
+// ]
+
+// products[0];
+// products[1];
+
+// const products = [15, 17, 12];
+const products = {
+    "0" : 15,
+    "1" : 17,
+    "2" : 12
+
+}
