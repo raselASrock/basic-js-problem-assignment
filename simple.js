2e1f59119ff0954d5888c9a76bd30ace34195d68
raselASrock <mdraselm413@gmail.com>
2022-08-12 21:53:06 +0600

    function-added

--- simple.js
+++ simple.js
@@ -10,6 +10,12 @@
 // }
 // console.log(sum);
 
-function(i){
-    
+function sum( i ){
+    if( i == 1){
+        return 1;
+    }
+    console.log(i)
+    return i + sum(i-1);
 }
+const result =sum(5);
+console.log(result);
