a395a2240d4393504bfff29bba081c5e5ac5b341
raselASrock <mdraselm413@gmail.com>
2022-08-12 21:23:43 +0600

    second-code-added

--- simple.js
+++ simple.js
@@ -1,5 +1,15 @@
-let sum = 0;
-for( i = 0; i <= 5; i++){
-    sum = sum + i;
+// let sum1 = 0;
+// for( i = 0; i <= 5; i++){
+//     sum = sum1 + i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for( i = 5; i>=1; i--){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+function(i){
+    
 }
-console.log(sum);
